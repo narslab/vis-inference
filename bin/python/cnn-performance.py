4b85c4b3ee28ef8468607604c21795c8af91caeb
Jimi Oke <folajimioke@gmail.com>
2021-02-24 17:43:13 -0500

    new results

--- bin/python/cnn-performance.py
+++ bin/python/cnn-performance.py
@@ -103,7 +103,7 @@ def trainModelWithDetailedMetrics(image_size, scenario, num_epochs = 10, trial_s
     
     # CALLBACKS
     model_metrics = Metrics(val_data=(validation_images, validation_labels))
-    early_stopping = EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
     
     # INIT MODEL AND PARAMS, FIT
     K.clear_session()
@@ -295,4 +295,4 @@ def getScenarioModelPerformance(res = 64, num_epochs = 15, seed_val = 1, test_bo
     return df
 
 if __name__ == "__main__":
-    getScenarioModelPerformance(res=128, num_epochs=20, seed_val = 111, test_boolean=False)
+    getScenarioModelPerformance(res=128, num_epochs=16, seed_val = 5, test_boolean=False)
