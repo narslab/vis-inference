f5e610f94cd0c535ea1c03d970b38edb494baa11
Nasko Apostolov <naskoap@gmail.com>
2021-03-16 02:51:48 -0400

    Save unlabeled images and generate index
    Generated unlabeled images, index and image classification csv files
    
    Added function saveUnlabeledImage to py script
    
    Updated labeled image directory

--- bin/python/label-image-files.py
+++ bin/python/label-image-files.py
@@ -9,9 +9,14 @@ import re # for matching image file name classes
 import matplotlib.pyplot as plt
 import random
 import ntpath
+import csv
+from timeit import default_timer as timer
 
 RAW_IMAGE_DIR = '../../data/raw/Pictures for AI'
-TIDY_IMAGE_DIR = '../../data/tidy/labeled_images/'
+TIDY_IMAGE_DIR = '../../data/tidy/unlabeled_images/'
+ARB_CLASS_DIR = '../../results/arborist_collective/'
+SAVE_NAME_DIR = '../../data/tidy/unlabeled_images/'
+FILE_NAME_DIR = '../../data/raw/Pictures for AI\\'
 
 def getListOfFiles(dirName):
     """Returns single list of the filepath of each of the training image files"""
@@ -53,6 +58,38 @@ def saveImageFiles(image_file_list):
     print('Number of improbable images saved:', improbable_counter - 1)    
     print('Number of possible images saved:', possible_counter - 1)
     print('Number of probable images saved:', probable_counter - 1)
+
+def saveUnlabeledImage(image_file_list):
+    """Save images to data/tidy/unlabeled_images, create image index and classification csv files"""
+    csv_col_index = ['Unlabeled Image', 'Original Classification']
+    csv_col_classifier = ['Image Name', 'Classification']
+    index = {}
+    classifier = []
+    counter = 1
+    if not os.path.exists(TIDY_IMAGE_DIR):
+        os.makedirs(TIDY_IMAGE_DIR)
+    for filename in image_file_list:
+        if '.JPG' in filename or '.jpg' in filename:        
+            save_name = TIDY_IMAGE_DIR + 'image' + '-' + str(counter) + '.jpg'
+            index[save_name] = filename
+            imageio.imwrite(save_name, np.array(Image.open(filename)))
+            counter += 1
+    if not os.path.exists(ARB_CLASS_DIR):
+        os.makedirs(ARB_CLASS_DIR)
+    with open(ARB_CLASS_DIR + 'labels_index.csv', 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=csv_col_index)
+        writer.writeheader()
+        for key in index.keys():
+            key_name = key.replace(SAVE_NAME_DIR,'')
+            classifier.append(key_name)
+            val_name = index[key].replace(FILE_NAME_DIR,'')
+            f.write("%s,%s\n"%(key_name,val_name))
+    classifier_dict = dict.fromkeys(classifier)
+    with open(ARB_CLASS_DIR + 'image_classification.csv', 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=csv_col_classifier)
+        writer.writeheader()
+        for key in classifier_dict.keys():
+            f.write("%s,%s\n"%(key,classifier_dict[key]))
 
 def plotRawImages(image_file_list, images_per_class = 2):
     fig, axarr = plt.subplots(images_per_class, 3, sharex=True, sharey=True)
