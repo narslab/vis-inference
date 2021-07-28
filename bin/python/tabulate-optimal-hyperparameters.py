220294252d5ab4aeae110c5fc1547ca377851d73
Jimi Oke <folajimioke@gmail.com>
2021-07-28 17:01:11 -0400

    added new hyperparam tables

--- bin/python/tabulate-optimal-hyperparameters.py
+++ bin/python/tabulate-optimal-hyperparameters.py
@@ -9,18 +9,24 @@ from helpers import *
 import numpy as np
 import pandas as pd
 
-RESOLUTION_LIST = [189, 252, 336] #, 384]
+IMAGE_WIDTH_LIST = [189, 252, 336] #, 384]
 SCENARIO_LIST = ["Pr_Im", "PrPo_Im", "Pr_PoIm", "Pr_Po_Im"]
 
-def getOptHyperparamsSummary(scenario_list, resolution_list, orientation="landscape"):
+def getOptHyperparamsSummary(scenario_list, width_list, rectangular=False, orientation="landscape"):
     df_all = pd.DataFrame()
     for s in scenario_list:
-        for p in resolution_list:
-            p_dict = getOptCNNHyperparams(p, p, s)
-            df = pd.DataFrame.from_dict(p_dict, orient='index', columns=["Values"])#, columns= ["Hyperparameters", "Values"])
+        for w in width_list:
+            if rectangular==True:
+                h = getRectangularImageHeight(w)
+                image_shape = 'rect'
+            else:
+                h = w
+                image_shape = 'square'
+            opt_dict = getOptCNNHyperparams(w, h, s) 
+            df = pd.DataFrame.from_dict(opt_dict, orient='index', columns=["Values"])#, columns= ["Hyperparameters", "Values"])
             print(df)
             df['Scenario'] = s
-            df['Resolution'] = p
+            df['Resolution'] = w
             df = df.drop(["tuner/bracket", "tuner/epochs", "tuner/initial_epoch", "tuner/round"])
             df_all = df_all.append(df)
     df_all['Hyperparameters'] = df_all.index
@@ -31,12 +37,12 @@ def getOptHyperparamsSummary(scenario_list, resolution_list, orientation="landsc
         df_all = df_all.pivot(index=['Hyperparameters'], columns=['Scenario','Resolution'], values='Values')
     elif orientation=="portrait":
         df_all = df_all.pivot(index=['Scenario','Hyperparameters'], columns=['Resolution'], values='Values')
-    file = '../../results/opt-hyperparameter-summary-table-revised-resolution-' + orientation + '.csv' # TODO
+    file = '../../results/opt-hyperparameter-summary-table-' + orientation + '-' + image_shape + '.csv' # TODO
     df_all.to_csv(file, index=True, float_format = ':.2E')
     return(df_all)
 
 
 if __name__ == "__main__":
-    #getOptHyperparamsSummary(SCENARIO_LIST, RESOLUTION_LIST)
-    getOptHyperparamsSummary(SCENARIO_LIST, RESOLUTION_LIST, orientation='portrait')
+    getOptHyperparamsSummary(SCENARIO_LIST, IMAGE_WIDTH_LIST, rectangular=False, orientation='portrait')
+    getOptHyperparamsSummary(SCENARIO_LIST, IMAGE_WIDTH_LIST, rectangular=False, orientation='landscape')
 
