8dfac7526ccc36b8c6b1629c97b9f02c0b318d3b
Jimi Oke <folajimioke@gmail.com>
2021-08-17 12:58:40 -0400

    updated plot-processed-images.py to fix explore-processed-images function as well as update new examples

--- bin/python/plot-processed-images.py
+++ bin/python/plot-processed-images.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 
 IMAGE_ARRAY = np.load('../../data/tidy/preprocessed-images/w-252px-h-252px-scenario-Pr_Po_Im-train.npy',allow_pickle = True)
+IMAGE_ARRAY_TEST = np.load('../../data/tidy/preprocessed-images/w-252px-h-252px-scenario-Pr_Po_Im-test.npy',allow_pickle = True)
 
 def getFigure2sub(images):
     pr = []
@@ -22,19 +23,24 @@ def getFigure2sub(images):
 
 def plotSelectedProcessedImages(image_array, save=True):
     ## Generates the subfigures in Figure 2 in the paper.
-    pr, po, im = getFigure2sub(image_array)
-    example_image_list = [pr[3], pr[25], 
-                          pr[91], po[17], 
-                          po[39], po[71], 
-                          im[23], im[45], im[87]]
+    # pr, po, im = getFigure2sub(image_array)
+    # example_image_list = [pr[3], pr[25], 
+    #                       pr[91], po[17], 
+    #                       po[39], po[71], 
+    #                       im[23], im[45], im[87]]
+
+    example_image_list = [764, 765, 252, 253,
+                          480, 481, 482, 483,
+                          898, 899, 548, 549]
     #print(example_image_list)
-    example_image_filenames = ['probable-example-1.png', 'probable-example-2.png',
-                               'probable-example-3.png', 'possible-example-1.png', 
-                               'possible-example-2.png', 'possible-example-3.png',
-                               'improbable-example-1.png', 'improbable-example-2.png',
-                               'improbable-example-3.png',                            
+    example_image_filenames = ['probable-example-1.png', 'probable-example-1-flipped.png',
+                               'probable-example-2.png', 'probable-example-2-flipped.png',
+                               'possible-example-1.png', 'possible-example-1-flipped.png',
+                               'possible-example-2.png', 'possible-example-2-flipped.png',
+                               'improbable-example-1.png', 'improbable-example-1-flipped.png',
+                               'improbable-example-2.png', 'improbable-example-2-flipped.png',
                               ]
-    for i,n in enumerate(example_image_list):
+    for i, n in enumerate(example_image_list):
         plt.imshow(image_array[n][0], cmap = 'gist_gray')
         #plt.imshow(np.rot90(image_array[n][0]), cmap = 'gist_gray')
         plt.tight_layout()
@@ -47,21 +53,36 @@ def plotSelectedProcessedImages(image_array, save=True):
 def exploreProcessedImages(image_array):
     rows= 15
     cols = 15
-    f, ax = plt.subplots(rows,cols, figsize=(50,30))
+    
     k = 0 #1120 #1120 #2240
-    for i in np.arange(rows):
-        for j in np.arange(cols):
-            ax[i][j].set_title(str(k))
-            ax[i][j].imshow(image_array[k][0])#, cmap = 'gist_gray')
-            ax[i][j].set_xticklabels([])
-            ax[i][j].set_yticklabels([])
-            k += 1
-            plt.show()
+    for b in np.arange(0, len(image_array), rows*cols):
+      f, ax = plt.subplots(rows, cols, figsize=(50,30))
+      while k < b + rows*cols:
+        is_looping = True
+        for i in np.arange(rows):
+            for j in np.arange(cols):
+                ax[i][j].set_title(str(k))
+                ax[i][j].imshow(image_array[k][0])#, cmap = 'gist_gray')
+                ax[i][j].set_xticklabels([])
+                ax[i][j].set_yticklabels([])
+                k  += 1
+                if k > len(image_array) - 1:
+                  is_looping = False
+                  print("Max k reached; breaking out of loop")
+                  break
+            if not is_looping:
+              print("Max k reached; breaking out of loop")
+              break
+        if not is_looping:
+          print("Max k reached; breaking out of loop")
+          break              
+      plt.show()
+      continue
     return
 
 def main():
-    #plotSelectedProcessedImages(IMAGE_ARRAY, save=False)
-    exploreProcessedImages(IMAGE_ARRAY)
+    plotSelectedProcessedImages(IMAGE_ARRAY, save=True)
+    #exploreProcessedImages(IMAGE_ARRAY)
     return
 
 if __name__ == "__main__":
